88d621f451c61f6c8cc91c1853a6d812fc4e4495
Carvalhoidk <tiagorafaelcarvalho1@gmail.com>
2025-04-08 01:32:04 +0100

    botao com props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import React from "react";
-import navbarButton from "./components/buttons/navbarButton"; // Caminho correto
+import NavbarButton from "./components/buttons/navbarButton"; // Caminho correto
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to the App</h1>
-        {/* Exibe o botão de login */}
-        <navbarButton />
+        <NavbarButton text="Sign In" />
+        <NavbarButton />
       </header>
     </div>
   );
--- frontend/src/components/buttons/navbarButton.js
+++ frontend/src/components/buttons/navbarButton.js
@@ -1,22 +1,23 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import { FaUser } from "react-icons/fa";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button"; // Importando o componente Button
+import { FaUser } from "react-icons/fa"; // Ícone de usuário
+import "bootstrap/dist/css/bootstrap.min.css"; // Importando o CSS do Bootstrap
 
-function navbarButton() {
+function NavbarButton({ text = "Login" }) {
+  // Definindo o texto como prop, com valor padrão "Login"
   return (
     <div className="d-flex justify-content-center mt-5">
       <Button
-        variant="light"
-        size="md"
-        className="text-dark"
-        onClick={() => alert("Login clicked!")}
+        variant="light" // Fundo branco
+        size="md" // Tamanho médio do botão
+        className="text-dark d-flex justify-content-between align-items-center" // Alinhamento flex
+        onClick={() => alert(`${text} clicked!`)}
       >
-        Login
+        {text} {/* Aqui o texto vem da prop */}
         <FaUser className="ms-2" /> {/* Ícone de login */}
       </Button>
     </div>
   );
 }
 
-export default navbarButton;
+export default NavbarButton;
