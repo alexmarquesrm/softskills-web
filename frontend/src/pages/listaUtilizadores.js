1c71c081965ab736de2d3fc0c5d9806bc046d56a
Alexandre Marques <alexandremarquesrm@gmail.com>
2025-04-09 01:23:33 +0100

    componente dataTable e lista de colaboradores corrigida

--- frontend/src/pages/listaUtilizadores.js
+++ frontend/src/pages/listaUtilizadores.js
@@ -1,19 +1,65 @@
-import "../App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import CustomNavbar from "../components/navbar/customNavbar";
 import Footer from "../components/footer/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Tabela from "../components/tabels/tabelaUsers";
+import DataTable from '../components/tables/dataTable';
 
-function App() {
+export default function UsersTable() {
+  const [tableRows, setTableRows] = useState([]);
+  const [error, setError] = useState(null);
+
+  const tableColumns = [
+    { field: 'id', headerName: 'ID', width: 100, headerAlign: 'left', disableColumnMenu: true },
+    { field: 'nome', headerName: 'Nome', flex: 1, headerAlign: 'left', disableColumnMenu: true },
+    { field: 'email', headerName: 'Email', flex: 1, headerAlign: 'left', disableColumnMenu: true },
+    { field: 'telefone', headerName: 'Telefone', flex: 0.7, headerAlign: 'left', disableColumnMenu: true },
+    { field: 'departamento', headerName: 'Departamento', flex: 0.7, headerAlign: 'left', disableColumnMenu: true },
+    { field: 'funcao', headerName: 'Função', flex: 1, headerAlign: 'left', disableColumnMenu: true },
+  ];
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/colaborador`);
+      console.log(response.data);
+      const utilizadores = response.data;
+
+      const sortedUtilizadores = utilizadores.sort((a, b) => a.colaborador_id - b.colaborador_id);
+
+            setTableRows(
+                sortedUtilizadores.map((colaborador) => ({
+                    key: colaborador.colaborador_id,
+                    id: colaborador.colaborador_id,
+                    nome: colaborador.nome,
+                    departamento: colaborador.departamento,
+                    funcao: colaborador.cargo,
+                    email: colaborador.email,
+                    telefone: colaborador.telefone,
+                }))
+            );
+
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div>
+    <div className="page-container">
       <CustomNavbar />
-      <Tabela/>
-     <Footer />
+      <div className="data-container">
+        <div style={{ marginBottom: '20px', paddingTop: '20px' }}/>
+        <div style={{ height: '65vh', width: '99%', overflowY: 'auto', paddingBottom: '40px', border: 'none', boxShadow: 'none' }}>
+          <DataTable rows={tableRows || []} columns={tableColumns} />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
-
-export default App;
- 
