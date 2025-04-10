dac40f3676b8d1de2872b311f04c4b41a6224244
Carvalhoidk <tiagorafaelcarvalho1@gmail.com>
2025-04-10 17:03:07 +0100

    fix basic text field

--- frontend/src/components/textFields/basic.js
+++ frontend/src/components/textFields/basic.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Form, Col, InputGroup } from 'react-bootstrap';
 
-const InputField = ({ label, type, placeholder, name, value, onChange, icon, colSize, rows, style, readOnly,disable }) => {
-
+const InputField = ({ label, type, placeholder, name, value, onChange, icon, colSize, rows, style, disable, readOnly }) => {
   return (
     <Col md={colSize}>
       <Form.Group controlId={`form${name}`}>
         <Form.Label>{label}</Form.Label>
         <InputGroup>
           {icon && <InputGroup.Text>{icon}</InputGroup.Text>}
-            <Form.Select 
-              name={name}
-              value={value}
-              onChange={onChange}
-              className="form-input"
-              style={{ ...style }}
-              readOnly={readOnly}
-              disabled={disable}
-            >
-            </Form.Select>
+          <Form.Control
+            as={type === 'textarea' ? 'textarea' : 'input'} 
+            rows={type === 'textarea' && rows ? rows : undefined}
+            placeholder={placeholder}
+            name={name}
+            value={value}
+            onChange={onChange}
+            className="form-input"
+            style={{ ...style}} 
+            readOnly={readOnly}
+            disabled={disable}
+          />
         </InputGroup>
       </Form.Group>
     </Col>
