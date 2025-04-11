24a683a1a86195ef5e84bc4c0e5d75960bbf4f8f
Alexandre Marques <alexandremarquesrm@gmail.com>
2025-04-11 17:27:28 +0100

    reorder to fix error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+// Pages & Components
+import LandingPage from "./pages/landingPage";
 import ListaUtilizadores from "./pages/userList";
-import PerfilUtilizador from "./pages/userProfile";;
+import PerfilUtilizador from "./pages/userProfile";
 import EditarUtilizadorGestor from "./pages/editarUtilizador_Gestor";
 import AdicionarUtilizadorGestor from "./pages/adicionarUtilizador_Gestor";
 import AdicionarFicheiroAssincronoFormador from "./modals/adicionarFicheiroAssincrono_Formador";
-
-import LandingPage from "./pages/landingPage";
-//import PagGestor from "./pages/pageGestor";
+// import PagGestor from "./pages/pageGestor";
 import CustomNavbar from "./components/navbar/customNavbar";
 import Footer from "./components/footer/footer";
-import ProtectedRoute from "./components/ProtectedRoute"; // Import the new component
-
+import ProtectedRoute from "./components/ProtectedRoute";
 function AppContent() {
     return (
         <div className="app-container">
@@ -30,7 +30,6 @@ function AppContent() {
                         <Route path="/gestor/editarPerfil" element={<EditarUtilizadorGestor />} />
                         <Route path="/gestor/adicionarPerfil" element={<AdicionarUtilizadorGestor />} />
                         <Route path="/formador/adicionarAssincrono" element={<AdicionarFicheiroAssincronoFormador />} />
-
                         {/* <Route path="/landing/gestor" element={<PagGestor />} /> */}
                     </Route>
 
