9b76c696700e3d46febec9edca83b8ac63dd8191
RodrigoJRMarques <rodrigomarquesrm.rm.rm@gmail.com>
2025-04-11 18:52:04 +0100

    Alteração do cardCourses e cardPedido, adicionado utilização do cardRow no cardCourses na landingPage

--- frontend/src/pages/landingPage.js
+++ frontend/src/pages/landingPage.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, } from "react";
 import axios from "../config/configAxios";
 import FeaturedCourses from "../components/cards/cardCourses";
+import CardRow from '../components/cards/cardRow';
 import PromoSection from "../components/promo/PromoSection";
 import StatsSection from "../components/stats/StatsSection";
+import { Container } from "react-bootstrap";
 
 function AppContent() {
   const [cursos, setCursos] = useState([]);
@@ -28,9 +30,16 @@ function AppContent() {
     fetchData();
   }, []);
 
+  const renderCourseCard = (curso, index) => (
+    <FeaturedCourses index={index} curso={curso} />
+  );
+
   return (
-    <div>
-      <FeaturedCourses cursos={cursos} />
+    <div style={{marginLeft: "300px", marginRight: "300px"}}>    
+      <Container className="my-4 px-500" style={{ maxWidth: '1650px' }}>
+      <h2>Cursos Em Destaque</h2>
+      <CardRow dados={cursos} renderCard={renderCourseCard} scrollable={false} colSize={4} />
+      </Container>
       <PromoSection />
       <StatsSection />
     </div>
