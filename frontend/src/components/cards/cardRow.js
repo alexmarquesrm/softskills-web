d3da4fd360efa62b224d88e2b3d6e401b481dc46
RodrigoJRMarques <rodrigomarquesrm.rm.rm@gmail.com>
2025-04-11 17:15:41 +0100

    Componetes cardRow e cardPedido criados

---
+++ frontend/src/components/cards/cardRow.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import "./cardRow.css";
+
+function CardRow({ dados = [], renderCard, colSize = 3, scrollable = false }) {
+    // Se scrollable for verdadeiro, usamos o layout horizontal
+    if (scrollable) {
+        return (
+            <div className="card-row-container">
+                {dados.map((item, index) => (
+                    <div key={index} className="card-row-item">
+                        {renderCard(item, index)}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    // Caso contrário, grid com colunas
+    return (
+        <Row className="g-3">
+            {dados.map((item, index) => (
+                <Col key={index} md={colSize} className="d-flex">
+                    <div className="w-100">
+                        {renderCard(item, index)}
+                    </div>
+                </Col>
+            ))}
+        </Row>
+    );
+}
+
+export default CardRow;
