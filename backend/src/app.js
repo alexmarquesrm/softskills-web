a1c492dc6da4d5ab48d0a6ef23aab226c75ae8d9
Alexandre Marques <alexandremarquesrm@gmail.com>
2025-05-26 17:08:55 +0100

    nova tentativa

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ const trabalhosRoutes = require('./routes/trabalhosRoutes');
 const quizzRoutes = require('./routes/quizz');
 
 const allowedOrigins = [
-  'https://hence-remainder-pressure-santa.trycloudflare.com',
+  'https://distance-timing-kits-resolution.trycloudflare.com',
   'http://localhost:3000'
 ];
 
