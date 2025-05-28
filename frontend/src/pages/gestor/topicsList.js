f3fe54b24eb1633c4c9b0924b148eedfbb3d7481
RodrigoJRMarques <rodrigomarquesrm.rm.rm@gmail.com>
2025-05-28 01:30:55 +0100

    fix categoria, area e topicos quando edita e sai nao aparece os dados de add

--- frontend/src/pages/gestor/topicsList.js
+++ frontend/src/pages/gestor/topicsList.js
@@ -174,7 +174,6 @@ export default function TopicsList() {
 
   const handleModalClose = () => {
     setShowModal(false);
-    setSelectedTopic(null);
   };
 
   const handleModalSuccess = (message) => {
