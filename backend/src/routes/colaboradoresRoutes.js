7580323dda0b89441c44ac2879651ba1ba25e8b2
Alexandre Marques <alexandremarquesrm@gmail.com>
2025-06-01 13:49:02 +0100

    fix routes

--- backend/src/routes/colaboradoresRoutes.js
+++ backend/src/routes/colaboradoresRoutes.js
@@ -9,6 +9,8 @@ router.get("/username/:username", controladorUtilizadores.getUserByLogin);
 router.post("/login", controladorUtilizadores.login);
 router.post("/registo", controladorUtilizadores.registarNovoColaborador);
 router.post("/reset-password", controladorUtilizadores.resetPassword);
+router.post("/google-login", controladorUtilizadores.googleLogin);
+
 router.post("/saudacao", controladorUtilizadores.getSaudacao);
 
 // Rotas mobile (sem autenticação)
@@ -20,9 +22,10 @@ router.post("/registrar-fcm-token", controladorMobile.registrarFcmToken);
 router.get("/me", authenticate, controladorUtilizadores.getMe);
 router.get("/", authenticate, controladorUtilizadores.getAllColaboradores);
 router.get("/:id", authenticate, validateResourceAccess, controladorUtilizadores.getColaboradorById);
+router.get('/token/:id', authenticate, validateResourceAccess, controladorUtilizadores.novoToken);
 router.post("/adicionar", authenticate, controladorUtilizadores.criarColaborador);
-router.put("/:id", authenticate, validateResourceAccess, controladorUtilizadores.updateColaborador);
-router.delete("/:id", authenticate, controladorUtilizadores.deleteColaborador);
+router.put("/atualizar/:id", authenticate, validateResourceAccess, controladorUtilizadores.updateColaborador);
+router.delete("/apagar/:id", authenticate, controladorUtilizadores.deleteColaborador);
 router.post("/change-password", authenticate, controladorUtilizadores.changePassword);
 
 // Rotas mobile protegidas
