5b6dbe27b8cc9556f20f57e149e390ec94274069
Alexandre Marques <alexandremarquesrm@gmail.com>
2025-06-01 20:33:30 +0100

    logs extra

--- frontend/src/pages/formandos/pageCourse.js
+++ frontend/src/pages/formandos/pageCourse.js
@@ -143,7 +143,7 @@ export default function CursoFormando() {
         const response = await axios.get(`/material/curso/${id}/materiais`, {
           headers: { Authorization: `${token}` }
         });
-        console.log('Response:', response.data);
+        console.log('Response:', response.data.data);
         if (response.data.success) {
           const materialsData = response.data.data;
 
